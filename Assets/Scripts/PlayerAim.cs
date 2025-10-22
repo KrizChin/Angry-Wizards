d1f4a9d4638b7425709b2ba643e1a5775735c61c
KrizChin <155257711+KrizChin@users.noreply.github.com>
2025-10-22 14:25:50 -0700

    Revert "Go back to a previous commit"
    This reverts commit e944470c10d3c53f608d029ad8286480048f779d.

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script rotates the player to face the mouse cursor allowing the player to aim.
+
 public class PlayerAim : MonoBehaviour
 {
     Camera mainCamera;
@@ -13,8 +15,13 @@ public class PlayerAim : MonoBehaviour
     void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Converts the mouse position from the mainCamera to game world.
         Vector2 direction = mousePos - transform.position;
+        //Calculates the direction vector from player to mouse to figure out where to rotate player.
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //Mathf.Atan2 gets the angle in radians and converts it into degrees.
+        //Compute the angle between the direction and x-axis using trigonometry.
         transform.rotation = Quaternion.Euler(0, 0, angle);
+        //Apply the rotation to the player on the z-axis.
     }
 }
